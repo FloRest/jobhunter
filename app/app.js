804e7b7ae39265f9f918c0d5cbbebe3b01690f42
Orlilol <florian.respaud@epitech.eu>
2015-01-05 13:41:50 +0800

    secret

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 var CONFIG = {};
 CONFIG.api = {
-  endpoint : "http://172.30.2.73",
+  endpoint : "http://localhost",
   port : "1338"
 };
 CONFIG.api.route = CONFIG.api.endpoint + ':' + CONFIG.api.port;
